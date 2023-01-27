79953eb8414139017fa4e623f10feaf832362e10
feh-franc0 <fernandofv1110@gmail.com>
2023-01-27 15:38:53 -0300

    colocando o prefixo node: quando é uma chamada interna e adicionando um atalho com watch para ficar de olho nas mudancas do codigo sem ter q startr o projeto toda hr

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import http from 'http'
+import http from 'node:http'
 
 const server = http.createServer((req, res) => {
-  return res.end('Hello World')
+  return res.end('Hello Mundo')
 })
 
 server.listen(3333)
